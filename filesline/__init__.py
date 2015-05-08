69509fdb101807b701b71b0ed520453263cf3d14
edonyM <edonyzpc@gmail.com>
2015-05-08 15:40:17 +0800

    pep8 style code

--- filesline/__init__.py
+++ filesline/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
  #        .---.         .-----------  
  #       /     \  __  /    ------    
@@ -21,41 +20,7 @@
  # Filename: __init__.py
  # 
  # Description: All Rights Are Reserved                 
-class pcolor:
-    ''' This class is for colored print in the python interpreter!
-    "F2" call Addpy() function to add this class which is defined
-    in the .vimrc for vim Editor.
-    
-    STYLE: \033['display model';'foreground';'background'm
-    DETAILS:
-    FOREGROUND        BACKGOUND       COLOR
-    ---------------------------------------
-    30                40              black
-    31                41              red
-    32                42              green
-    33                43              yellow
-    34                44              blue
-    35                45              purple
-    36                46              cyan
-    37                47              white
-    DISPLAY MODEL    DETAILS
-    -------------------------
-    0                default
-    1                highlight
-    4                underline
-    5                flicker
-    7                reverse
-    8                non-visiable
-    
-    e.g：
-    \033[1;31;40m   <!--1-highlight;31-foreground red;40-background black-->
-    \033[0m         <!--set all into default-->
-    '''
-    WARNING = '\033[0;37;41m'
-    ENDC = '\033[0m'
-    def disable(self):
-        self.ENDC = ''
-        self.WARNING = ''
- 
-__all__=['filesline','pcolor','fileline']
 
+from __future__ import absolute_import
+
+from . import filesline
