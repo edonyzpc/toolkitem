ec6d4042f13a641975bbcd2598e76b0cefef0b54
edonyM <edonyzpc@gmail.com>
2015-05-28 22:30:23 +0800

    add extractor.py

---
+++ fileparser/extractor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+r"""
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2015-05-28 22:20
+ #
+ # Filename: extractor.py
+ #
+ # Description: All Rights Are Reserved
+ #
+ # ******
+ # Extract the specific content from text with the given keys.
+"""
+import os
+import re
+class Extractor(object):
+    """
+    Extract the specific content with keys.
+    """
+    def __init__(self, keys, extracted_file):
+        self.keys = keys
+        self.extracted_file
