16da1f548567784204242f202c5cca29591ad578
edonyM <edonyzpc@gmail.com>
2015-05-29 16:19:57 +0800

    add extractor.py

--- fileparser/extractor.py
+++ fileparser/extractor.py
@@ -32,6 +32,49 @@ class Extractor(object):
     """
     Extract the specific content with keys.
     """
-    def __init__(self, keys, extracted_file):
-        self.keys = keys
-        self.extracted_file
+    def __init__(self, keys, extracted_file, output_file=None, flag=None):
+        if type(keys) is list:
+            self.keys = keys
+        elif type(keys) is str:
+            self.keys =[keys]
+        else:
+            raise ValueError("Wrong Key type")
+        if output_file:
+            self.output_file = output_file
+        else:
+            self.output_file = 'EXTRACT'
+        if flag:
+            self.flag = [0, 0]  #flag are controlling add the keys into write file
+        else:
+            self.flag = flag
+        self.pattern = Extractor.re_pattern(self.keys)
+        self.extracted_file = extracted_file
+
+    @staticmethod
+    def re_pattern(keys):
+        if len(keys) > 2:
+            raise ValueError("The keys are too much, simplify them less than 2.\n")
+        regular_expression = keys[0] + '(?P<con>.*)'
+        if len(keys) == 2:
+            regular_expression += keys[1]
+        return re.compile(regular_expression)
+
+    def parser(self):
+        with open(self.output_file, 'w') as out:
+            with open(self.extracted_file) as exfile:
+                for line in exfile.readlines():
+                    g = self.pattern.search(line)
+                    if g:
+                        if self.flag[0]:
+                            print('test')
+                            out.write(self.keys[0])
+                        out.write(g.group('con'))
+                        if self.flag[1]:
+                            out.write(self.keys[1])
+                        out.write('\n')
+        print('Finish Extract')
+
+if __name__ == '__main__':
+    tmp = Extractor('http:', 'career_old')
+    tmp.flag = [1,0]
+    tmp.parser()
