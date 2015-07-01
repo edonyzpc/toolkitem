73c005553076b2516f67215ceacf3bd0582a9574
edonyM <edonyzpc@gmail.com>
2015-07-01 20:31:08 +0800

    add the shell command to remove elf files

--- emsh/rm_gcc_exe_file.sh
+++ emsh/rm_gcc_exe_file.sh
@@ -1,1 +1,16 @@
-rm $(ls -la | grep '^-rwx.*' | sed 's/^-rwx.*[0-9] //g')
+rmlist=$(ls -la | grep '^-rwxrwxr-x.*' | sed 's/^-rwx.*[0-9] //g')
+echo $rmlist
+echo "Remove?"
+read check
+yes="y"
+if [ "$check" == "$yes" ]
+then
+    if [ ! -n "$rmlist" ]
+    then
+        echo "No Need to Remove"
+    else
+        rm $rmlist
+    fi
+else
+    echo "No Remove"
+fi
