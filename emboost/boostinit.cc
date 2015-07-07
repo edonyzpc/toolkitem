c3e8f3845d044c31656f1e398ed03267a1600b5b
edonyM <edonyzpc@gmail.com>
2015-07-08 02:36:07 +0800

    test boost python

--- emboost/boostinit.cc
+++ emboost/boostinit.cc
@@ -1,41 +1,45 @@
-//#include <boost/noncopyable.hpp>
-//#include <boost/python.hpp>
-
-//class pyinit: boost::noncopyable {
-//    public:
-//        pyinit(int initsigs = 1) {
-//            assert(initsigs == 0 || initsigs == 1);
-//            Py_InitializeEx(initsigs);
-//        }
-//        ~pyinit(){};
-//
-//        bool isInitialized() {
-//            return Py_IsInitialized();
-//        }
-//        const char* version() {
-//            return Py_GetVersion();
-//        }
-//};
-//
-//int main() {
-//    pyinit pinit;
-//
-//    python::list l;
-//    l.append("edony");
-//    l.append(12);
-//
-//    cout << len(l) << endl;
-//}
-//#cat test.cpp 
-#include <boost/lexical_cast.hpp>
+#include <boost/noncopyable.hpp>
+#include <boost/python.hpp>
 #include <iostream>
 
-int main()
-{
-    using boost::lexical_cast;
-    int a= lexical_cast<int>("123456");
-    double b = lexical_cast<double>("123.456");
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    return 0;
+using namespace std;
+using namespace boost;
+
+class pyinit: boost::noncopyable {
+    public:
+        pyinit(int initsigs = 1) {
+            assert(initsigs == 0 || initsigs == 1);
+            Py_InitializeEx(initsigs);
+        }
+        ~pyinit(){};
+
+        bool isInitialized() {
+            return Py_IsInitialized();
+        }
+        const char* version() {
+            return Py_GetVersion();
+        }
+};
+
+int main() {
+    pyinit pinit;
+
+    boost::python::list l;
+    l.append("edony");
+    l.append(12);
+
+    cout << boost::python::len(l) << endl;
 }
+//#cat test.cpp 
+//#include <boost/lexical_cast.hpp>
+//#include <iostream>
+//
+//int main()
+//{
+//    using boost::lexical_cast;
+//    int a= lexical_cast<int>("123456");
+//    double b = lexical_cast<double>("123.456");
+//    std::cout << a << std::endl;
+//    std::cout << b << std::endl;
+//    return 0;
+//}
