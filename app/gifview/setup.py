a46e09e23e1fbc8dae1d615ee544203240afcc17
edonyM <edonyzpc@gmail.com>
2015-07-27 19:49:14 +0800

    fix the py2exe in Windows

--- app/gifview/setup.py
+++ app/gifview/setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 import py2exe
 
-setup(windows=['gifviewer.py', 'filemanage.py'])
+#setup(windows=['gifviewer.py', 'filemanage.py'])
+setup(windows=['gifviewer.py'])
