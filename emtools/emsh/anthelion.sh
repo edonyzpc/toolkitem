60081d2ab13ea86fda5759b6f246fb5060301e1d
edonyM <edonyzpc@gmail.com>
2016-01-05 23:28:27 +0800

    add automatic script

---
+++ emtools/emsh/anthelion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# author:edony
+date > test.edony
+chmod 777 /home/edony/code/github/toolkitem/emtools/trigger.py
+/home/edony/code/github/toolkitem/emtools/trigger.py
+chmod 666 /home/edony/code/github/toolkitem/emtools/trigger.py
