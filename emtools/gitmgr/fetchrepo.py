447bab1a03b6ad2987c64f0dab4e6fec00a60d94
edonyM <edonyzpc@gmail.com>
2016-10-19 23:09:12 +0800

    add update forked github repository tool

---
+++ emtools/gitmgr/fetchrepo.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ #        .---.         .-----------
+ #       /     \  __  /    ------
+ #      / /     \(  )/    -----   (`-')  _ _(`-')              <-. (`-')_
+ #     //////    '\/ `   ---      ( OO).-/( (OO ).->     .->      \( OO) )     .->
+ #    //// / //  :   : ---      (,------. \    .'_ (`-')----. ,--./ ,--/  ,--.'  ,-.
+ #   // /   /  / `\/ '--         |  .---' '`'-..__)( OO).-. ' |   \ |  | (`-')'.'  /
+ #  //          //..\\\\          (|  '--.  |  |  ' |( _) | | | |  . '|  |)(OO \    /
+ # ============UU====UU====      |  .--'  |  |  / : \|  |)| | |  |\    |  |  /   /)
+ #             '//||\\\`          |  `---. |  '-'  /  '  '-' ' |  | \   |  `-/   /`
+ #               ''``            `------' `------'    `-----' `--'  `--'    `--'
+ # ######################################################################################
+ #
+ # Author: edony - edonyzpc@gmail.com
+ #
+ # twitter : @edonyzpc
+ #
+ # Last modified: 2016-10-19 21:18
+ #
+ # Filename: fetchrepo.py
+ #
+ # Description: All Rights Are Reserved
+ #
+"""
+#import scipy as sp
+#import math as m
+#import matplotlib as mpl
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D as Ax3
+#from scipy import stats as st
+#from matplotlib import cm
+#import numpy as np
+
+class PyColor(object):
+    """ This class is for colored print in the python interpreter!
+    "F3" call Addpy() function to add this class which is defined
+    in the .vimrc for vim Editor."""
+    def __init__(self):
+        self.self_doc = """
+        STYLE: \033['display model';'foreground';'background'm
+        DETAILS:
+        FOREGROUND        BACKGOUND       COLOR
+        ---------------------------------------
+        30                40              black
+        31                41              red
+        32                42              green
+        33                43              yellow
+        34                44              blue
+        35                45              purple
+        36                46              cyan
+        37                47              white
+        DISPLAY MODEL    DETAILS
+        -------------------------
+        0                default
+        1                highlight
+        4                underline
+        5                flicker
+        7                reverse
+        8                non-visiable
+        e.g:
+        \033[1;31;40m   <!--1-highlight;31-foreground red;40-background black-->
+        \033[0m         <!--set all into default-->
+        """
+        self.warningcolor = '\033[0;31m'
+        self.tipcolor = '\033[0;32m'
+        self.endcolor = '\033[0m'
+        self._newcolor = ''
+    @property
+    def new(self):
+        """
+        Customized Python Print Color.
+        """
+        return self._newcolor
+    @new.setter
+    def new(self, color_str):
+        """
+        New Color.
+        """
+        self._newcolor = color_str
+    def disable(self):
+        """
+        Disable Color Print.
+        """
+        self.warningcolor = ''
+        self.endcolor = ''
+
+from urllib.request import urlopen
+import subprocess as sp
+
+def get_repo_url(repo_path):
+    cmd = 'cd ' + repo_path + ';git remote -v'
+    (status, output) = sp.getstatusoutput(cmd)
+    tmp = [item for item in output.split() if item.find('git@github.com:edonyM') > -1]
+    if len(set(tmp)) > 1:
+        print('You have more than one repository URL address')
+        return None
+    else:
+        url_addr = 'https://github.com/' + tmp[0].split(':')[1]
+        if status > -1:
+            return url_addr
+        else:
+            print('Wrong repository directory!')
+            return None
+
+
+def repo_web(url_addr):
+    buf = urlopen(url_addr).readlines()
+    for line in buf:
+        if line.decode('utf-8').find('forked from') > -1:
+            web_buf = line.decode('utf-8')
+            break
+        else:
+            web_buf = None
+    if web_buf is None:
+        return None
+    else:
+        return 'https://github.com/' + web_buf.split('"')[3]
+
+
+if __name__ == "__main__":
+    furl = repo_web(get_repo_url('~/coding/script-utility'))
+    print(furl)
+    furl1 = repo_web(get_repo_url('./'))
+    print(furl1)
