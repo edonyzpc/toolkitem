3e4d910232130b6e544db839884f37ad5770a25b
edonyM <edonyzpc@gmail.com>
2017-03-01 18:21:29 +0800

    fix test case

--- tests/test_tmp.py
+++ tests/test_tmp.py
@@ -1,8 +1,8 @@
 class TestClass:
     def test_one(self):
         x = "this"
-        assert 'i' in x
+        assert('i' in x)
 
     def test_two(self):
         x = "hello"
-        assert hasattr(x, 'hello')
+        assert(False == hasattr(x, 'hello'))
