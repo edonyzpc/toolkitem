4efb9b422b5838c50950774da61b812253d123a4
edonyM <edonyzpc@gmail.com>
2017-03-26 00:59:34 +0800

    update the test of pycolor module

--- tests/test_pycolor.py
+++ tests/test_pycolor.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
-from pycolor import PyColor
-from pycolor import cprint
+import sys
+sys.path.append('./')
+from colorprinter.pycolor import PyColor
+from colorprinter.pycolor import cprint
 
 @PyColor('ured')
 def printer(string):
@@ -19,6 +21,21 @@ class TestClass(object):
         py_color = PyColor('green')
         py_color.format = 'ucyan'
         cprint(py_color.format, 'this is test')
+
+    def test_disableformat(self):
+        py_color = PyColor('ured')
+        cprint(py_color.format, 'this is test')
+        py_color.disable()
+        cprint(py_color.format, 'this is disable')
+        assert py_color.format is ''
+
+    def test_colorstr(self):
+        str1 = 'this is a test'
+        py_color = PyColor('green')
+        str2 = py_color.colorstr(str1)
+        str3 = py_color.colorstr(str1, 'red')
+        assert((str2 == '\033[0;32;40mthis is a test\033[0m') and
+               (str3 == '\033[0;31;40mthis is a test\033[0m'))
 
 #if __name__ == "__main__":
 #    cprint('ugreen', 'hello edony')
