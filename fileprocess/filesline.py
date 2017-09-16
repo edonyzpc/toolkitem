86c505fbbc74756cf42ea2ab848a8f859abe0762
edonyM <edonyzpc@gmail.com>
2017-09-16 18:45:13 +0800

    init files line generator child class

--- fileprocess/filesline.py
+++ fileprocess/filesline.py
@@ -28,8 +28,20 @@ import sys
 import os
 from dirlist import DirList
 
+class FilesLine(DirList):
+    """generate the line number of files located in directory
+    """
+    def __init__(self, directory):
+        super(FilesLine, self).__init__(directory)
+        self.filesline = 0
+
+    def sum_lines(self):
+        pass
+
 if __name__ == "__main__":
     tmp = DirList('/Users/edony/coding/toolkitem')
-    print(tmp.dirlist)
-    print(sys.path)
-    print(os.path.split(os.path.realpath(__file__)))
+    #print(tmp.dirlist)
+    #print(sys.path)
+    #print(os.path.split(os.path.realpath(__file__)))
+    tmp1 = FilesLine('/Users/edony/coding/toolkitem')
+    print(tmp1.dirlist)
