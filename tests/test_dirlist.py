87befd342ae6d50805ccee4957c6c08e5265e140
edonyM <edonyzpc@gmail.com>
2017-09-18 23:19:16 +0800

    add _listdir test code

--- tests/test_dirlist.py
+++ tests/test_dirlist.py
@@ -11,3 +11,18 @@ class TestClass(object):
     def test_DirList(self):
         curdir = os.path.split(os.path.realpath(__file__))[0]
         tmp = DirList(curdir)
+
+    def test__listdir(self):
+        with pytest.raises(Exception):
+            tmp = DirList(None)
+
+    def test__listdir_none_root(self):
+        curdir = os.path.split(os.path.realpath(__file__))[0]
+        tmp = DirList(curdir)
+        tmp.root = None
+        tmp._listdir(curdir)
+
+    def test_getattr(self):
+        assert DirList.getattr(__file__) == 'FILE'
+        assert DirList.getattr(os.path.split(os.path.realpath(__file__))[0]) == 'DIR'
+        assert DirList.getattr('/dev/ttyxxxx')
