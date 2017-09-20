210be00a55361336479fb2e7cfaf833afc91489a
edonyM <edonyzpc@gmail.com>
2017-09-20 21:17:43 +0800

    optimize sum_lines with concurrence algorithm to speedup

--- fileprocess/filesline.py
+++ fileprocess/filesline.py
@@ -26,26 +26,39 @@ r"""
 """
 import sys
 import os
+from functools import reduce
+from multiprocessing.pool import Pool
 from dirlist import DirList
 
 class FilesLine(DirList):
     """generate the line number of files located in directory
     """
-    def __init__(self, directory):
+    def __init__(self, directory, CPURES=8):
         super(FilesLine, self).__init__(directory)
         self.filesline = 0
+        self.MAX_RES = CPURES
 
-    def sum_lines(self):
-        # TODO(edony): optimize algorithm of sum_lines method
+    def _count_filelines(self, file):
+        with open(file, 'rb') as filebuf:
+            return len(filebuf.readlines())
+
+    def _adder(self, a, b):
+        return a + b
+
+    def sum_lines(self, SPEEDUP=True):
         filesname = []
         for item_dir in self.dirlist.keys():
             for item_file in self.dirlist[item_dir][1]:
                 filesname.append(item_dir + '/' + item_file)
-        for filename in filesname:
-            with open(filename, 'rb') as filebuf:
-                self.filesline += len(filebuf.readlines())
 
-        return self.filesline
+        if SPEEDUP:
+            with Pool(self.MAX_RES) as res_pool:
+                return reduce(self._adder, res_pool.map(self._count_filelines, filesname))
+        else:
+            for filename in filesname:
+                with open(filename, 'rb') as filebuf:
+                    self.filesline += len(filebuf.readlines())
+            return self.filesline
 
 """ comment this line to test with __main__
 if __name__ == "__main__":
@@ -55,8 +68,12 @@ if __name__ == "__main__":
     #print(sys.path)
     #print(os.path.split(os.path.realpath(__file__)))
     tmp1 = FilesLine('/Users/edony/coding/toolkitem')
-    print(tmp1.dirlist)
+    print("==== Not Speedup ====")
+    print(time.time())
+    tmp1.sum_lines(SPEEDUP=False)
+    print(time.time())
+    print("==== Do Speedup ====")
     print(time.time())
     tmp1.sum_lines()
     print(time.time())
-comment this line to test with __main__ """
+#comment this line to test with __main__ """
