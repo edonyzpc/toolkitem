23b557a1b3d3a817b81d0ebfb77f231c903ad569
edonyM <edonyzpc@gmail.com>
2017-09-20 21:26:59 +0800

    simplify directory structure of fileprocess module files

--- fileprocess/dirlist/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['dirlist', 'DirList']
-import sys
-
-if sys.version_info.major == 3:
-    from dirlist.dirlist import DirList
-elif sys.version_info.major == 2:
-    from dirlist import DirList
