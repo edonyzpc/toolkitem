5005488f36bf350dce294e9d9d0867bbcf14b558
edonyM <edonyzpc@gmail.com>
2017-10-15 23:50:05 +0800

    add syncrepo class test

--- tests/test_syncrepo.py
+++ tests/test_syncrepo.py
@@ -38,5 +38,14 @@ from gitmgr import SyncUpstreamRepo
 class TestClass:
     def test_syncrepo(self):
         os.system('git clone https://edonyM:123456789@github.com/edonyM/TPM2.0-TSS.git')
+        os.system('cp gitmgr/log_config.ini ./')
         syncrepo('./TPM2.0-TSS', 'https://github.com/01org/tpm2-tss.git')
-        os.system('rm -rf ../TPM2.0-TSS')
+        os.chdir("../")
+        os.system('rm -rf ./TPM2.0-TSS ./log_config.ini')
+
+    def test_SyncUpstreamRepo(self):
+        os.system('git clone https://edonyM:123456789@github.com/edonyM/TPM2.0-TSS.git')
+        os.system('cp gitmgr/log_config.ini ./')
+        syncer = SyncUpstreamRepo('./TPM2.0-TSS', 'https://github.com/01org/tpm2-tss.git')
+        syncer.sync_upstream()
+        os.system('rm -rf ../TPM2.0-TSS ../log_config.ini')
